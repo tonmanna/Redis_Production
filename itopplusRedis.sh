4eae550c4a1e76993346afc6947c7d90b69436bc
Worawut <tonmanna@gmail.com>
2018-03-19 18:25:15 +0700

    change config default

--- itopplusRedis.sh
+++ itopplusRedis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sysctl -w net.core.somaxconn=20000
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+#echo never > /sys/kernel/mm/transparent_hugepage/enabled
 redis-server /etc/redis.conf
