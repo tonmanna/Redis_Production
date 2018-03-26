5cb20bd3f0195cf55094534b00649c2a8390b6dc
Worawut <tonmanna@gmail.com>
2018-03-26 13:14:37 +0700

    AOF RDM Disable

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f myredis
-docker run -d -p 6379:6379 --restart=always --privileged -v $(pwd)/redis.conf:/etc/redis.conf -v redis_content:/data --name myredis redis_itopplus
+docker run -d -p 6379:6379 --restart=always --privileged -v redis_content:/data --name myredis redis_itopplus
