ecca1570ad9286cf176d0b682845e6806450422d
Worawut Boontan <worawut@theiconweb.com>
2024-04-18 18:40:37 +0700

    Chang Run Command

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL EMAIL="IT_TEAM <it@itopplus.com>"
 COPY ./redis.conf /etc/redis.conf
 COPY itopplusRedis.sh /usr/local/bin/itopplusRedis.sh
 WORKDIR /usr/local/bin
-COPY ./tests/tls /data/tls
+# COPY ./tests/tls /data/tls
 # RUN mkdir /data
 RUN chmod 755 itopplusRedis.sh
 CMD ["./itopplusRedis.sh"]
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f myredis
-docker run -d -p 6399:6379 --restart=always --privileged -v redis_content:/data --name myredis redis_itopplus
+docker run -d -p 6399:6379 --restart=always --privileged -v /${PWD}/redis.conf:/etc/redis.conf -v redis_content:/data --name myredis redis_itopplus
